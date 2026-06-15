df9e6c74a6cf31641021fcc0e6ae2c8bfbfb7868
HeckinNerd <nacnudemcd@gmail.com>
2026-06-15 13:14:34 -0700

    Delete kubejs/server_scripts/customrecipegui/generated directory

--- kubejs/server_scripts/customrecipegui/generated/crushing_tfmg_coal_coke_dust.js
+++
@@ -1,9 +0,0 @@
-// [customrecipe] type=Crushing | out=tfmg:coal_coke_dust | in=tfmg:coal_coke | time=200
-ServerEvents.recipes(event => {
-  event.recipes.create.crushing(
-    [
-      'tfmg:coal_coke_dust'
-    ],
-    'tfmg:coal_coke'
-  ).processingTime(200)
-})
